db0df65909c7df8578185e30c5ba62575bcf3e96
Matthew Johnson <matthew.johnson@digital.hmrc.gov.uk>
2016-11-23 17:43:32 +0000

    DDCNLS-303 changes to fake app

--- test/WithFakeApplication.scala
+++ test/WithFakeApplication.scala
@@ -23,7 +23,7 @@ trait WithFakeApplication extends BeforeAndAfterAll {
 
   //lazy val fakeApplication = FakeApplication()
 
-  implicit val app: Application = new GuiceApplicationBuilder().build()
+  implicit def app: Application = new GuiceApplicationBuilder().build()
     //.configure(additionalConfiguration)
     //.bindings(bindModules:_*).in(Mode.Test)
 
