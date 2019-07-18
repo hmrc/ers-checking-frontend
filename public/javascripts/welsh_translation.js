482ec2fa981f51fdbdafbd031a77fb126687b154
MarkHunter2 <mark.hunter2@digital.hmrc.gov.uk>
2019-07-18 15:25:00 +0100

    DDCNLS-4362 : Introduce welsh translation of JS generated content

--- public/javascripts/welsh_translation.js
+++ public/javascripts/welsh_translation.js
@@ -8,30 +8,30 @@ function initLocalisedContent() {
       "ogl.link.text" : "Open Government Licence v3.0",
       "ogl.pt.2" : ", except where otherwise stated",
       "crown.copyright" : "© Crown Copyright",
-      "deskproAjax.received.pt1" : "We have received your contact details",
-      "deskproAjax.received.pt2" : "Our technical team will send you an email to let you know when you can access the service again.",
-      "deskproAjax.not.received.pt1" : "Sorry, we are unable to receive your contact details right now",
-      "deskproAjax.not.received.pt2" : "Please try again later or email",
-      "deskproAjax.not.received.pt3" : "if you need help with this service.",
-      "upload.uploaded" : "Report uploaded – now check it for errors",
-      "upload.cancel" : "Cancel",
-      "upload.remove" : "Remove report",
-      "upload.not.supported" : "This service doesn't currently support mobile devices"
+      "csv.already.chosen" : "Choose a different file &ndash; you've already chosen one with this name",
+      "csv.file.too.large" : "Check this file (you can only upload files that are {0}MB or less)",
+      "csv.file.wrong.type" : "Choose a different file &ndash; it must be a CSV file",
+      "csv.file.name.too.long" : "The filename must contain {0} characters or less",
+      "csv.file.name.invalid.chars" : "Choose a different file &ndash; the file's name can't contain invalid characters",
+      "ods.file.too.large" : "The attached file is to large. We only accept files less than {0}MB",
+      "ods.file.wrong.type" : "Choose a different file &ndash; it must be an ODS file",
+      "ods.file.name.too.long" : "Choose a different file &ndash; the file's name must be {0} characters or less",
+      "ods.file.name.invalid.chars" : "Choose a different file &ndash; the file's name can't contain invalid characters"
     },
     "cy" : {
       "ogl.pt.1" : "Mae'r holl gynnwys ar gael dan y",
       "ogl.link.text" : "Drwydded Llywodraeth Agored, fersiwn 3.0",
       "ogl.pt.2" : ", oni nodir yn wahanol",
       "crown.copyright" : "© Hawlfraint y Goron",
-      "deskproAjax.received.pt1" : "Mae’ch manylion cyswllt wedi dod i law",
-      "deskproAjax.received.pt2" : "Bydd ein tîm technegol yn anfon e-bost atoch i roi gwybod i chi pryd gallwch gael mynediad at y gwasanaeth eto.",
-      "deskproAjax.not.received.pt1" : "Mae’n ddrwg gennym. Ni allwn gymryd eich manylion cyswllt ar hyn o bryd",
-      "deskproAjax.not.received.pt2" : "Rhowch gynnig arall arni yn nes ymlaen neu e-bostiwch",
-      "deskproAjax.not.received.pt3" : "os oes angen help arnoch gyda’r gwasanaeth hon.",
-      "upload.uploaded" : "Adroddiad wedi’i uwchlwytho - nawr ewch ati i’w wirio am wallau",
-      "upload.cancel" : "Canslo",
-      "upload.remove" : "Dileu’r adroddiad",
-      "upload.not.supported" : "Nid yw’r gwasanaeth hwn yn cefnogi dyfeisiau symudol ar hyn o bryd"
+      "csv.already.chosen" : "Dewiswch ffeil wahanol &ndash; rydych eisoes wedi dewis un gyda’r enw hwn",
+      "csv.file.too.large" : "Gwiriwch y ffeil hon (gallwch ond uwchlwytho ffeiliau sy’n {0}MB neu lai)",
+      "csv.file.wrong.type" : "Dewiswch ffeil wahanol &ndash; rhaid iddi fod yn ffeil CSV",
+      "csv.file.name.too.long" : "Mae’n rhaid i enw’r ffeil gynnwys {0} o gymeriadau neu lai",
+      "csv.file.name.invalid.chars" : "Dewiswch ffeil wahanol &ndash; na all enw’r ffeil gynnwys cymeriadau annilys",
+      "ods.file.too.large" : "Mae’r ffeil a atodwyd yn rhy fawr. Rydym dim ond yn derbyn ffeiliau sy’n llai na {0}MB",
+      "ods.file.wrong.type" : "Dewiswch ffeil wahanol – rhaid iddi fod yn ffeil ODS",
+      "ods.file.name.too.long" : "Dewiswch ffeil wahanol – rhaid i enw’r ffeil fod yn {0} o gymeriadau neu lai",
+      "ods.file.name.invalid.chars" : "Dewiswch ffeil wahanol &ndash; na all enw’r ffeil gynnwys cymeriadau annilys"
     }
   }
 
@@ -65,8 +65,6 @@ function initLocalisedContent() {
 $(document).ready(function () {
 
   initLocalisedContent();
-
-  console.log("BANG", GOVUK.playLanguage);
 
   // Switch out OGL footer and Crown Copyright if welsh language
   if (GOVUK.playLanguage === "cy") {
