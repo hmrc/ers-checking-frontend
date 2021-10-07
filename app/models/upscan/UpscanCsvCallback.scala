d8634b45c620a7858b9136c7387f8c8080aeb887
LetMeTestThat <40994998+LetMeTestThat@users.noreply.github.com>
2021-10-07 12:36:27 +0100

    [EH][DDCE-2053] tmp: trying to get type match right

--- app/models/upscan/UpscanCsvCallback.scala
+++ app/models/upscan/UpscanCsvCallback.scala
@@ -17,7 +17,6 @@
 package models.upscan
 
 import play.api.libs.json.{Format, Json}
-import play.api.http.FileMimeTypes
 
 case class UpscanCsvFilesCallback(uploadId: UploadId, uploadStatus: UploadStatus = NotStarted) {
   def isStarted: Boolean = uploadStatus != NotStarted
@@ -38,8 +37,7 @@ case class UpscanCsvFilesCallbackList(files: List[UpscanCsvFilesCallback]){
     _.uploadStatus.isInstanceOf[UploadedSuccessfully]
   }
 
-  def areAnyFilesWrongMimeType(): Boolean = files.contains(UpscanCsvFilesCallback(_, FailedMimeType))
-
+  def areAnyFilesWrongMimeType(): Boolean = files.filter(x => x.uploadStatus == FailedMimeType).nonEmpty
 }
 
 object UpscanCsvFilesCallbackList {
