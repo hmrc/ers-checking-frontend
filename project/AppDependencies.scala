f39e1df5eea622326d34007f5b8545c98af5bbf3
Paul Callaghan <83694362+PaulC-TDS@users.noreply.github.com>
2024-06-05 12:59:39 +0100

    DDCE-5532: Play frontend upgrade (#151)
    * DDCE-5532: Play frontend upgrade
    
    * DDCE-5532: PR bot feedback and removed template

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,23 +4,23 @@ import sbt.*
 object AppDependencies {
 
   val pekkoVersion = "1.0.2"
-  val bootstrapVersion = "8.5.0"
-  val mongoVersion = "1.8.0"
+  val bootstrapVersion = "8.6.0"
+  val mongoVersion = "1.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "commons-io"                  %  "commons-io"                   % "2.15.1",
+    "commons-io"                  %  "commons-io"                   % "2.16.1",
     "org.apache.pekko"            %% "pekko-stream"                 % pekkoVersion,
     "org.apache.pekko"            %% "pekko-connectors-csv"         % pekkoVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-play-30"           % mongoVersion,
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"   % bootstrapVersion,
     "uk.gov.hmrc"                 %% "domain-play-30"               % "9.0.0",
     "uk.gov.hmrc"                 %% "tabular-data-validator"       % "1.8.0",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"   % "9.4.0"
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"   % "9.11.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.wiremock"           %  "wiremock-standalone"     % "3.4.2",
+    "org.wiremock"           %  "wiremock-standalone"     % "3.6.0",
     "org.apache.pekko"       %% "pekko-testkit"            % pekkoVersion,
     "org.jsoup"              %  "jsoup"                   % "1.17.2",
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion,
