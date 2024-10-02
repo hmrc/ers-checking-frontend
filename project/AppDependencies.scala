acaedd943b75d682f36838a2d3135fc3aa7af168
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2024-10-02 19:32:47 +0100

    DDCE-5794 - updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,25 +4,25 @@ import sbt.*
 object AppDependencies {
 
   val pekkoVersion = "1.0.2"
-  val bootstrapVersion = "9.0.0"
+  val bootstrapVersion = "9.5.0"
   val mongoVersion = "1.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "commons-io"                  %  "commons-io"                   % "2.16.1",
-    "org.apache.pekko"            %% "pekko-stream"                 % pekkoVersion,
+    "org.apache.pekko"            %% "pekko-stream"                 % "1.0.3",
     "org.apache.pekko"            %% "pekko-connectors-csv"         % pekkoVersion,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-play-30"           % mongoVersion,
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"                 %% "domain-play-30"               % "9.0.0",
+    "uk.gov.hmrc"                 %% "domain-play-30"               % "10.0.0",
     "uk.gov.hmrc"                 %% "tabular-data-validator"       % "1.8.0",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"   % "9.11.0"
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"   % "10.12.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.wiremock"           %  "wiremock-standalone"     % "3.6.0",
-    "org.apache.pekko"       %% "pekko-testkit"            % pekkoVersion,
-    "org.jsoup"              %  "jsoup"                   % "1.17.2",
+    "org.wiremock"           %  "wiremock-standalone"     % "3.9.1",
+    "org.apache.pekko"       %% "pekko-testkit"            % "1.0.3",
+    "org.jsoup"              %  "jsoup"                   % "1.18.1",
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoVersion,
   ).map(_ % Test)
