5955d5d85865407592c0d1b4746ac2831caa491b
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-03-26 11:01:56 +0000

    DDCE-6520 - pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val pekkoVersion = "1.0.3"
   val bootstrapVersion = "9.11.0"
-  val mongoVersion = "2.5.0"
+  val mongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
@@ -16,7 +16,7 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"   % bootstrapVersion,
     "uk.gov.hmrc"                 %% "domain-play-30"               % "10.0.0",
     "uk.gov.hmrc"                 %% "tabular-data-validator"       % "1.8.0",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"   % "11.12.0"
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"   % "11.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
