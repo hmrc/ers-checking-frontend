319d5da5d431ac067106a560c707e53d072ab178
harish-korada1 <174133905+harish-korada1@users.noreply.github.com>
2025-04-01 11:20:50 +0100

    DDCE-6337: updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,7 +16,7 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-30"   % bootstrapVersion,
     "uk.gov.hmrc"                 %% "domain-play-30"               % "10.0.0",
     "uk.gov.hmrc"                 %% "tabular-data-validator"       % "1.8.0",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"   % "11.12.0"
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc-play-30"   % "11.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
