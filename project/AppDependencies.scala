4e40ddb890f0e0515339d9fa62edc24b1b7da053
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-10-15 12:25:12 +0100

    DDCE-3162 - pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.2.0"
-  private val mongoVersion = "2.9.0"
+  private val mongoVersion = "2.10.0"
 
   private val compile: Seq[ModuleID] = Seq(
     ws,
