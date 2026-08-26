664179290572c88c6114f8893e061ad074ec6a69
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2026-08-26 12:30:48 +0100

    DDCE-9086 - updating feedback url/adding service navigation

--- app/config/ApplicationConfig.scala
+++ app/config/ApplicationConfig.scala
@@ -57,7 +57,8 @@ class ApplicationConfig @Inject() (config: ServicesConfig) {
 
   lazy val loginPath: String                 = Option(config.getString("govuk-tax.auth.login_path")).getOrElse("sign-in")
   lazy val signIn: String                    = s"$basGatewayHost/bas-gateway/$loginPath"
-  lazy val signOut: String                   = s"$basGatewayHost/bas-gateway/sign-out-without-state"
+  lazy val feedBackUrl: String               = s"${config.getString("microservice.services.feedback-survey-frontend.url")}?useServiceNavigation"
+  lazy val signOut: String                   = getSignOutUrl(feedBackUrl)
   lazy val timeOut: String                   = s"$loginCallback/signed-out"
   lazy val dassGatewayHost: String           = config.getString("govuk-tax.dass-gateway.host")
   lazy val dassAgentClientsPath: String      = s"$dassGatewayHost/ers/agent/clients"
