037ec5992931a0cd6ef0bb5c6cb2cbe79ca6fa33
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2026-09-01 17:02:51 +0100

    Formatting

--- test/utils/SchemeVersionSelectorSpec.scala
+++ test/utils/SchemeVersionSelectorSpec.scala
@@ -25,20 +25,31 @@ class SchemeVersionSelectorSpec extends AnyWordSpecLike with Matchers {
   "getSchemeVersion" should {
 
     "return All when parsed true for useV4andV5Scheme and useV6andV7Scheme" in {
-      SchemeVersionSelector.getSchemeVersion(useV4andV5Scheme = true, useV6andV7Scheme = true) shouldBe SchemeVersion.All
+      SchemeVersionSelector.getSchemeVersion(
+        useV4andV5Scheme = true,
+        useV6andV7Scheme = true
+      ) shouldBe SchemeVersion.All
     }
 
     "return V4andV5 when parsed true for useV4andV5Scheme and false for useV6andV7Scheme" in {
-      SchemeVersionSelector.getSchemeVersion(useV4andV5Scheme = true, useV6andV7Scheme = false) shouldBe SchemeVersion.V4andV5
+      SchemeVersionSelector.getSchemeVersion(
+        useV4andV5Scheme = true,
+        useV6andV7Scheme = false
+      ) shouldBe SchemeVersion.V4andV5
     }
 
     "return V6andV7 when parsed false for useV4andV5Scheme and true for useV6andV7Scheme" in {
-      SchemeVersionSelector.getSchemeVersion(useV4andV5Scheme = false, useV6andV7Scheme = true) shouldBe SchemeVersion.V6andV7
+      SchemeVersionSelector.getSchemeVersion(
+        useV4andV5Scheme = false,
+        useV6andV7Scheme = true
+      ) shouldBe SchemeVersion.V6andV7
     }
 
     "return V6andV7 when parsed false for useV4andV5Scheme and useV6andV7Scheme" in {
-      SchemeVersionSelector.getSchemeVersion(useV4andV5Scheme = false, useV6andV7Scheme = false) shouldBe SchemeVersion
-        .V4andV5
+      SchemeVersionSelector.getSchemeVersion(
+        useV4andV5Scheme = false,
+        useV6andV7Scheme = false
+      ) shouldBe SchemeVersion.V4andV5
     }
   }
 
